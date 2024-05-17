89eb4d689a75bb7665cdce8a7bffb7b11091af5f
TobMoeller <tobiasmoellerw@t-online.de>
2024-05-17 21:54:46 +0200

    remove unnecessary enum method

--- app/Enums/TimespanUnit.php
+++ app/Enums/TimespanUnit.php
@@ -2,21 +2,9 @@
 
 namespace App\Enums;
 
-use Illuminate\Support\Carbon;
-
 enum TimespanUnit: string
 {
     case DAY = 'day';
     case MONTH = 'month';
     case YEAR = 'year';
-
-    public function isValidRecordedAtDate(Carbon $date): bool
-    {
-        return $date->isStartOfDay() &&
-            match ($this) {
-                self::MONTH => $date->day === 1,
-                self::YEAR => $date->dayOfYear === 1,
-                default => true,
-            };
-    }
 }
