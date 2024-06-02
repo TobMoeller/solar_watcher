fcf1944e82b0e8ad98f9fe1e31b8263c1d1da7fb
TobMoeller <tobiasmoellerw@t-online.de>
2024-06-02 23:42:23 +0200

    inverter status idc field can be negative

--- app/Http/Requests/Api/V1/UpdateInverterStatusRequest.php
+++ app/Http/Requests/Api/V1/UpdateInverterStatusRequest.php
@@ -15,7 +15,7 @@ class UpdateInverterStatusRequest extends FormRequest
             'inverter_id' => ['required', 'int', 'exists:inverters,id'],
             'is_online' => ['required', 'bool'],
             'udc' => ['sometimes', 'nullable', 'decimal:0,2', 'between:0,1000000'],
-            'idc' => ['sometimes', 'nullable', 'decimal:0,2', 'between:0,1000000'],
+            'idc' => ['sometimes', 'nullable', 'decimal:0,2', 'between:-10,1000000'],
             'pac' => ['sometimes', 'nullable', 'decimal:0,2', 'between:0,1000000'],
             'pdc' => ['sometimes', 'nullable', 'decimal:0,2', 'between:0,1000000'],
         ];
